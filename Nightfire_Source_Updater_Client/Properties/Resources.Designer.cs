0e443919610d1ceba9f8fdae59751de0a0b37a12
colistro123 <colistro123@aol.com>
2018-10-03 18:14:30 -0300

    Remove SteamWorks.NET reference

--- Nightfire_Source_Updater_Client/Properties/Resources.Designer.cs
+++ Nightfire_Source_Updater_Client/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Nightfire_Source_Updater_Client.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Steamworks_NET {
-            get {
-                object obj = ResourceManager.GetObject("Steamworks_NET", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
